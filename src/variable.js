8e800e563bd08ea8680dc8660f8984b43dbd1e25
Alona-Isko <79259446+Alona-Isko@users.noreply.github.com>
2021-07-24 10:23:49 +0200

    рендерінг розмітки

---
+++ src/variable.js
@@ -0,0 +1,19 @@
+//     // - 1 - _ Var1
+// const imagesContainer = document.querySelector('.js-gallery');
+
+// const createGalleryItem = ({ preview, description }) => 
+//     `
+//     <li>
+//     <img src = "${preview}"
+//     alt = "${description}"
+//     width = "320">
+//     </li>
+//    `;
+
+// const gallery = galleryItems.reduce(
+//   (acc, image) =>
+//     acc + createGalleryItem(image),
+//   ' '
+// );
+
+// imagesContainer.insertAdjacentHTML('afterbegin', gallery);
